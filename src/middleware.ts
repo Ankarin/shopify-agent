0adbc85af15d38f0628b6365b4618d3cba249307
mitry <ankarn41k@gmail.com>
2025-10-10 17:43:31 +0300

    save

--- src/middleware.ts
+++ src/middleware.ts
@@ -33,7 +33,7 @@ export default clerkMiddleware(async (auth, req) => {
 
 export const config = {
     matcher: [
-        '/((?!_next|chat|iframe-test|widget|api/chat|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-        '/api/((?!chat).*)',
+        '/((?!_next|chat|iframe-test|widget|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+        '/api/organizations/:path*',
     ],
 };
